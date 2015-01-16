07a5d334b1120cb4d85464267eb1889c238ea7c7
Anthony Callaert <callaertanthony@gmail.com>
2015-01-16 19:25:40 +0100

    init piece abstract class

---
+++ Tetris/Tetris/Piece/Piece.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris.Pieces
+{
+    abstract class Piece
+    {
+        private int[,] shape;
+        private int indice_rotation;
+        private int coordonnee_x;
+        private int coordonnee_y;
+
+        public Piece()
+        {
+            this.shape = new int[4, 4];
+
+        }
+
+        public void placerDansCoin()
+        {
+
+        }
+
+        abstract private void initShape();
+    }
+}
