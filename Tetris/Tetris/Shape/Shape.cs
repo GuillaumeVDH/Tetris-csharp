6c64aef54d90d0a2093030234fd7f5e06c7ade54
Anthony Callaert <callaertanthony@gmail.com>
2015-01-16 19:26:30 +0100

    init shape class

---
+++ Tetris/Tetris/Shape/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris.Shape
+{
+    abstract class Shape
+    {
+        int[,] shape;
+
+        public Shape(){
+            this.shape = new int[4,4];
+        }
+    }
+}
