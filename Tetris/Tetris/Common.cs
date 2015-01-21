d1d95f2f8a4a92c6796738db791d084a4dfe0896
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2015-01-21 21:53:09 +0100

    Added some doc for Common values

--- Tetris/Tetris/Common.cs
+++ Tetris/Tetris/Common.cs
@@ -7,16 +7,16 @@ namespace Tetris
 {
     class Common
     {
-
-        static public int boardStartX = 100;
-        static public int boardStartY = 40;
-        static public int boardWidth = 200;
-        static public int boardHeight = 480;
-        static public int boardSizeX = 10;
-        static public int boardSizeY = 22;
+        static public int boardStartX = 100; //The start position - in pixel - of the board for the Y axis
+        static public int boardStartY = 40; //The start position - in pixel - of the board for the Y axis
+        static public int boardWidth = 200; //The width - in pixel - of the board for the Y axis
+        static public int boardHeight = 480; //The height - in pixel - of the board for the Y axis
+        static public int boardSizeX = 10; //The size - in case - of the board for the X axis
+        static public int boardSizeY = 22; //The size - in case - of the board for the Y axis
         
-        static public int blockTextureSize = 20;
+        static public int blockTextureSize = 20; //The size - in pixel - of a block texture (X == Y)
 
+        //Texture full path for each block
         static public string blockTextureI = "Blocks/BlockI";
         static public string blockTextureJ = "Blocks/BlockJ";
         static public string blockTextureL = "Blocks/BlockL";
